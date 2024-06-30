f8a60d1ea8555d0f1d508d138b12c0d68218399b
iSuckAtNames <chargebattery7@gmail.com>
2024-06-30 19:09:21 -0400

    Hopefully the Carbon's Skillbook crashes are finally dealt with

--- BasicMod-master (1)/BasicMod-master/src/main/java/brainlets/relics/CarbonsSkillbook.java
+++ BasicMod-master (1)/BasicMod-master/src/main/java/brainlets/relics/CarbonsSkillbook.java
@@ -2,7 +2,6 @@ package brainlets.relics;
 
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.RelicLibrary;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
@@ -11,8 +10,6 @@ import com.megacrit.cardcrawl.rewards.RewardItem;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.evacipated.cardcrawl.mod.stslib.patches.bothInterfaces.OnCreateCardInterface;
 
-import java.util.ArrayList;
-
 import static brainlets.BasicMod.makeID;
 
 public class CarbonsSkillbook extends BaseRelic implements OnCreateCardInterface{
@@ -20,6 +17,7 @@ public class CarbonsSkillbook extends BaseRelic implements OnCreateCardInterface
     public static final String ID = makeID(NAME); //This adds the mod's prefix to the relic ID, resulting in modID:MyRelic
     private static final RelicTier RARITY = RelicTier.BOSS; //The relic's rarity.
     private static final LandingSound SOUND = LandingSound.FLAT; //The sound played when the relic is clicked.
+    private static boolean shouldUpdate = false;
 
     private static boolean receivedShard;
 
@@ -42,23 +40,26 @@ public class CarbonsSkillbook extends BaseRelic implements OnCreateCardInterface
 
     public void onEquip() {
         receivedShard = false;
+        shouldUpdate = true;
     }
 
     public void update() {
         super.update();
-        if (AbstractDungeon.id != null) {
-            if (AbstractDungeon.getCurrRoom() != null) {
-                if (AbstractDungeon.player.hasRelic(PrismaticShard.ID)) {
-                    receivedShard = true;
-                }
-                if (!AbstractDungeon.isScreenUp && !receivedShard) {
-                    AbstractDungeon.combatRewardScreen.open();
-                    AbstractDungeon.combatRewardScreen.rewards.clear();
-                    AbstractDungeon.combatRewardScreen.rewards.add(new RewardItem(RelicLibrary.getRelic(PrismaticShard.ID).makeCopy()));
-                    receivedShard = true;
-                    AbstractDungeon.shopRelicPool.removeIf(id -> id.equals(PrismaticShard.ID));
-                    AbstractDungeon.combatRewardScreen.positionRewards();
-                    (AbstractDungeon.getCurrRoom()).rewardPopOutTimer = 0.25F;
+        if (shouldUpdate) {
+            if (AbstractDungeon.id != null) {
+                if (AbstractDungeon.getCurrRoom() != null) {
+                    if (AbstractDungeon.player.hasRelic(PrismaticShard.ID)) {
+                        receivedShard = true;
+                    }
+                    if (!AbstractDungeon.isScreenUp && !receivedShard) {
+                        AbstractDungeon.combatRewardScreen.open();
+                        AbstractDungeon.combatRewardScreen.rewards.clear();
+                        AbstractDungeon.combatRewardScreen.rewards.add(new RewardItem(RelicLibrary.getRelic(PrismaticShard.ID).makeCopy()));
+                        receivedShard = true;
+                        AbstractDungeon.shopRelicPool.removeIf(id -> id.equals(PrismaticShard.ID));
+                        AbstractDungeon.combatRewardScreen.positionRewards();
+                        (AbstractDungeon.getCurrRoom()).rewardPopOutTimer = 0.25F;
+                    }
                 }
             }
         }
